6b3e128c500f2988f8f73aa8fccc54485bea0fd6
Jacob Rushinski <dsrush2@verizon.net>
2026-04-22 10:03:49 -0400

    force push

--- apps/api/src/common/common.module.ts
+++ apps/api/src/common/common.module.ts
@@ -1,6 +1,4 @@
 import { Global, Module } from '@nestjs/common';
-import { DatabaseModule } from '../core/database/database.module';
-import { CsrfController } from './middleware/csrf/csrf.controller';
 import { OriginResolverService } from './services/origin-resolver.service';
 import { RoleContextService } from './services/role-context.service';
 import { SecurityConfig } from '../config/security.config';
@@ -11,8 +9,6 @@ import { SecurityConfig } from '../config/security.config';
  */
 @Global()
 @Module({
-  imports: [DatabaseModule],
-  controllers: [CsrfController],
   providers: [OriginResolverService, RoleContextService, SecurityConfig],
   exports: [OriginResolverService, RoleContextService, SecurityConfig],
 })
